200fef0023ddb86f2de8ec5da8d52a1cc333207a
MikelSage <michael.sagapolutele@yahoo.com>
2017-10-23 09:34:14 -0600

    complete user story 1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const $ = require('jQuery');
+const top_word = require('./top_word');
 
 document.addEventListener("DOMContentLoaded", () => {
   
--- src/top_word.js
+++ src/top_word.js
@@ -4,10 +4,14 @@ function getTopWord() {
   $.ajax({
     url: "https://wordwatch-api.herokuapp.com/api/v1/top_word",
     method: "GET"
-  }).then((data) => {displayTopWord(data)}
+  }).then((data) => {displayTopWord(data.word)}
   )
 }
 
 function displayTopWord(word_data) {
-  $('.top-word h3')
+  for (key in word_data) {
+    $('.top-word h3').text(`Top word from Word Watch API: ${key} (${word_data[key]})`)
+  }
 }
+
+module.exports = getTopWord();
