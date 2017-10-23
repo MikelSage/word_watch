1a4e801aff0583aa17fb8fe02816e519399da796
MikelSage <michael.sagapolutele@yahoo.com>
2017-10-23 10:12:36 -0600

    add countWords, modify captureWords to remove extra whitespace

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const top_word = require('./top_word');
+const breakdown = require('./submit_words');
 
 document.addEventListener("DOMContentLoaded", () => {
   top_word.getTopWord();
+  breakdown.breakdownListener();
 })
--- src/submit_words.js
+++ src/submit_words.js
@@ -6,9 +6,31 @@ function breakdownListener() {
 
 function breakdownWords() {
   let words = captureWords();
-  displayWords(words);
+  let counted_words = countWords(words);
+  debugger
+  displayWords(counted_words);
 }
 
 function captureWords() {
-  return $('.text-submission textarea').val().split(' ')
+  let raw_words = $('.text-submission textarea').val()
+  return raw_words.replace(/\s+/g, " ").trim().split(' ')
 }
+
+function countWords(words) {
+  let counter = {}
+  words.forEach((word) => {
+    counter[word] = (counter[word] || 0) + 1
+  })
+  return counter
+}
+
+function displayWords(words) {
+  let $article = $('word-count')
+  words.forEach((word) => {
+    $article.append(
+      `<p style="font"></p>`
+    )
+  })
+}
+
+module.exports = {breakdownListener};
